b3fdf86d2b78312c2193f8d617f5d67eb84c3e65
Maycon Pedroso Macario <160797266+Basara24@users.noreply.github.com>
2024-11-26 17:14:58 -0300

    CONECÇAO SPRING BOOT

---
+++ src/main/java/br/propjeto_tech4/tech4/Tech4Application.java
@@ -0,0 +1,13 @@
+package br.propjeto_tech4.tech4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tech4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tech4Application.class, args);
+	}
+
+}
---
+++ src/test/java/br/propjeto_tech4/tech4/Tech4ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.propjeto_tech4.tech4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tech4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

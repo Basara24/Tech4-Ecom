eb53f98aae766956c2fdd83a7e620cf8d186c69f
Maycon Pedroso Macario <160797266+Basara24@users.noreply.github.com>
2024-11-30 01:00:40 -0300

    metodo post Item add

--- src/main/java/br/propjeto_tech4/tech4/controller/ItemController.java
+++ src/main/java/br/propjeto_tech4/tech4/controller/ItemController.java
@@ -1,11 +1,10 @@
 package br.propjeto_tech4.tech4.controller;
 
+import br.propjeto_tech4.tech4.dto.ItemRequestDTO;
 import br.propjeto_tech4.tech4.modal.Item;
 import br.propjeto_tech4.tech4.repository.ItemRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -20,4 +19,21 @@ public class ItemController {
     public List<Item> findAll() {
         return repository.findAll(); // Usa o repositório para buscar os itens no banco
     }
+
+    @GetMapping("/{id}")
+    public Item findById(@PathVariable Integer id) {
+        return this.repository.findById(id)
+                .orElseThrow(() ->
+                        new IllegalArgumentException("Smartphone nao encontrado"));
+    }
+
+    @PostMapping
+    public Item save(@RequestBody ItemRequestDTO dto){
+        Item item = new Item();
+        item.setDescricao(dto.descricao());
+        item.setPreco(dto.preco());
+
+       return this.repository.save(item);
+    }
+
 }
---
+++ src/main/java/br/propjeto_tech4/tech4/dto/ItemRequestDTO.java
@@ -0,0 +1,6 @@
+package br.propjeto_tech4.tech4.dto;
+
+import java.math.BigDecimal;
+
+public record ItemRequestDTO(String descricao, BigDecimal preco) {
+}

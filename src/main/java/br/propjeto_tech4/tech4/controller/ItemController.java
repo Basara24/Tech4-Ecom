3b1865fdcf7b6d448cc149762ed9cd798b3074d1
Maycon Pedroso Macario <160797266+Basara24@users.noreply.github.com>
2024-11-30 01:42:18 -0300

    validacao post Item

--- src/main/java/br/propjeto_tech4/tech4/controller/ItemController.java
+++ src/main/java/br/propjeto_tech4/tech4/controller/ItemController.java
@@ -4,6 +4,7 @@ import br.propjeto_tech4.tech4.dto.ItemRequestDTO;
 import br.propjeto_tech4.tech4.modal.Item;
 import br.propjeto_tech4.tech4.repository.ItemRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -16,8 +17,10 @@ public class ItemController {
     private ItemRepository repository;
 
     @GetMapping
-    public List<Item> findAll() {
-        return repository.findAll(); // Usa o repositório para buscar os itens no banco
+    public ResponseEntity <List<Item>> findAll() {
+        List<Item> items = repository.findAll(); // Usa o repositório para buscar os itens no banco
+        return ResponseEntity.ok(items);
+
     }
 
     @GetMapping("/{id}")
@@ -28,12 +31,26 @@ public class ItemController {
     }
 
     @PostMapping
-    public Item save(@RequestBody ItemRequestDTO dto){
+    public ResponseEntity<Object> save(@RequestBody ItemRequestDTO dto) {
+        // Validação do preço
+        if ((dto.preco() == null)) {
+            return ResponseEntity.status(400).body("O preço deve ser maior que zero.");
+        }
+
+        // Validação da descrição
+        if (dto.descricao() == null || dto.descricao().trim().isEmpty()) {
+            return ResponseEntity.status(400).body("A descrição não pode estar vazia.");
+        }
+
+        // Criação do objeto Item
         Item item = new Item();
         item.setDescricao(dto.descricao());
         item.setPreco(dto.preco());
 
-       return this.repository.save(item);
-    }
+        // Salvando no banco de dados
+        this.repository.save(item);
 
+        // Retornando o objeto salvo
+        return ResponseEntity.status(201).body(item);
+    }
 }
